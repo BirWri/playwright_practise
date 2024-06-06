7eed2eca75066742716dc1e47289b188a3017e54
BirWri <55280458+BirWri@users.noreply.github.com>
2024-06-06 14:39:51 +0200

    Verify the number of posts test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -80,3 +80,51 @@ test.describe('About page', () => {
     
 })
 
+
+test.describe('Contact page', () => {
+    test('Open contact page, fill and submit the "Send Us Message" form', async ({ page }) => {
+        // Open URL
+        await page.goto('https://practice.sdetunicorns.com/');
+
+        // Click on Contact
+        await page.locator('#menu-item-493').click()
+
+        // Find and fill the forms inputs name, email, phone and message
+        await page.locator('#evf-277-field_ys0GeZISRs-1').fill('Ro Wright')
+        await page.locator('#evf-277-field_LbH5NxasXM-2').fill('janedoe@gmail.com')
+        await page.locator('#evf-277-field_66FR384cge-3').fill('2222222222')
+        await page.locator('#evf-277-field_yhGx3FOwr2-4').fill('Hello, greate job!')
+
+        // Submit the form
+        await page.locator('#evf-submit-277').click()
+
+        // Verify confirmation text
+         // Find the text locator "Thanks for contacting us! We will be in touch with you shortly"
+         const confirmationText = page.locator('text=Thanks for contacting us! We will be in touch with you shortly');
+
+         // Verify the heading is there and correct
+         await expect(confirmationText).toBeVisible();
+
+    })
+    
+})
+
+
+test.describe('Blog page', () => {
+    test('Verify the number of blog posts and the length of the blog post titles to be above 10 char', async ({ page }) => {
+        //Open url
+        await page.goto('https://practice.sdetunicorns.com/')
+
+        // Click on Blog
+        await page.locator('#menu-item-490').click()
+
+        // Retrieve the number of blog posts
+        const numberOfBlogPosts = page.locator('//section[@id="recent-posts-3"] // ul //li')
+
+        // Verify the number of posts
+        await expect(numberOfBlogPosts).toHaveCount(5);
+    })
+
+})
+
+
