4e249c29c916ae8e2498fae398b5aeb0521b0a3a
BirWri <55280458+BirWri@users.noreply.github.com>
2024-08-23 13:42:34 +0200

    multiple state and roles

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -17,3 +17,18 @@ test.describe('My account', () => {
     }) 
     
 })
+
+test.describe('Account Page', () => {
+    //test.use({storageState: 'notLoggedInState.json'})
+    test.use({ storageState: 'notLoggedInState.json' })
+
+    test('Verify login and register is visible', async ({ page }) => {
+        await page.goto('/my-account')
+        await expect(page.locator(`form[class*="login"]`)).toBeVisible()
+        await expect(page.locator(`form[class*="register"]`)).toBeVisible()
+        
+    })
+    
+    
+})
+
--- utils/global-setup.ts
+++ utils/global-setup.ts
@@ -5,6 +5,10 @@ async function globalSetup(config:FullConfig) {
     const page = await browser.newPage()
 
     await page.goto('https://practice.sdetunicorns.com/my-account')
+    //await page.context().storageState({path: 'notLoggedInState.json'})
+    await page.context().storageState({ path: 'notLoggedInState.json' });
+
+    //login
     await page.locator('#username').fill('tadare9944@chaladas.com')
     await page.locator('#password').fill('Practicepass1!')
     await page.locator('[value="Log in"]').click()
@@ -14,5 +18,7 @@ async function globalSetup(config:FullConfig) {
     await browser.close()
 
 }
+
+
 
 export default globalSetup;
