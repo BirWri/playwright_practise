0d6795c86999f1451a6609f8c6b89011f0ab3fbd
BirWri <55280458+BirWri@users.noreply.github.com>
2024-08-29 10:34:17 +0200

    use api to provide user info to fill in a from

--- tests/contact.api.spec.ts
+++ tests/contact.api.spec.ts
@@ -1,12 +1,12 @@
-import { test, expect, APIRequestContext } from '@playwright/test';
+import { test, expect, APIRequestContext, APIResponse } from '@playwright/test';
 import ContactPage from '../pages/contacts.page';
-import { faker } from '@faker-js/faker';
 
 
 
 test.describe('Contact page', () => {
     let contactPage : ContactPage;
     let fakerAPI: APIRequestContext;
+    let randomPerson : APIResponse;
 
     test.beforeAll(async ({ playwright }) => {
         fakerAPI = await playwright.request.newContext({
@@ -14,7 +14,8 @@ test.describe('Contact page', () => {
         });
         
         const response = await fakerAPI.get('users');
-        console.log(await response.json());
+        const responseBody = (await response.json());
+        randomPerson = responseBody[0];
     })
     
 
@@ -29,7 +30,12 @@ test.describe('Contact page', () => {
     test('Open contact page, fill and submit the "Send Us Message" form', async ({ page }) => {
 
         // Find and fill the forms inputs name, email, phone and message
-        await contactPage.fillForm(faker.person.fullName(), faker.internet.email(), faker.phone.number(), faker.lorem.paragraph(2));
+        await contactPage.fillForm(
+            randomPerson['name'],
+            randomPerson['email'],
+            randomPerson['phone'],
+            randomPerson['website']
+        );
 
          // Verify the heading is there and correct
          await expect(contactPage.confirmationText).toBeVisible();
